7632ffbc390876a37583b4aba806217acd0459ff
Edward Roddriguez <edwardR36@hotmail.com>
2018-11-15 20:25:21 -0500

    Add Assignment 3 to Assignments list

--- Assignments/js/assignments.js
+++ Assignments/js/assignments.js
@@ -3,5 +3,6 @@ $.getJSON('data.json', function(data) {
     //var assignments = [data.Assignments[0].name, data.Assignments[0].description];
     $('#asgmt1').append(data.Assignments[0].description);
     $('#asgmt2').append(data.Assignments[1].description);
+    $('#asgmt3').append(data.Assignments[2].description);
     console.log("its working");
   });
